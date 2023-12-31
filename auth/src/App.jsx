d07d685bea1c4668db27db3c8cd59115802662fe
YassineIDR <yassineidrissi2005@gmail.com>
2023-12-31 17:52:15 +0100

    final touches

--- auth/src/App.jsx
+++ auth/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
 import Signup from './components/Signup'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <div >
       <Signup/>
-    </>
+    </div>
   )
 }
 
